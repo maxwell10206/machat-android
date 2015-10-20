9fd70ca0cfdb3f177eba803dba005382437ec247
Maxwell <maxwell10206@sbcglobal.net>
2015-10-20 14:53:46 -0500

    Added Re-send message feature

--- app/src/main/java/machat/machat/main/dialogs/FavoriteItemDialogFragment.java
+++ app/src/main/java/machat/machat/main/dialogs/FavoriteItemDialogFragment.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import machat.machat.models.FavoriteItem;
 import machat.machat.R;
@@ -13,6 +14,7 @@ import machat.machat.R;
 public class FavoriteItemDialogFragment extends DialogFragment {
 
     public static final String FAVORITE_ITEM = "favoriteItem";
+
     private OnCompleteListener listener;
 
     @Override
@@ -22,7 +24,7 @@ public class FavoriteItemDialogFragment extends DialogFragment {
     }
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         final FavoriteItem favoriteItem = (FavoriteItem) getArguments().getSerializable(FAVORITE_ITEM);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
